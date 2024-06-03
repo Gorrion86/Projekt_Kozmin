878e74e664ab39206c4ef1ff244cbd2ff4f4c9b0
Kamil <kamil.gajowniczek@gmail.com>
2024-06-03 22:20:11 +0200

    empty email registration attempt test added

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,12 +2,15 @@ from helpers.support_functions import *
 from selenium.webdriver.common.by import By
 from test_data import login_data
 
+
 class LoginPageLocators:
     customer_login_content = (By.ID, "customer_login")
     username = (By.ID, "username")
     password = (By.ID, "password")
     login_button = (By.XPATH, '//*[@id="customer_login"]/div[1]/form/p[3]/button')
-    login_error_alert = (By.XPATH, '//*[@id="content"]/div/div[1]/ul')
+    register_button = (By.XPATH, '//*[@id="customer_login"]/div[2]/form/p[3]/button')
+    wrong_login_alert = (By.XPATH, '//*[@class="woocommerce-error"]/li[contains(.,"nieprawidłowa nazwa użytkownika lub hasło.")]')
+    empty_email_registration_alert = (By.XPATH, '//*[@class="woocommerce-error"]/li[contains(.,"Proszę podać poprawny adres e-mail.")]')
 
 
 def login_page_content_visible(driver_instance):
@@ -16,17 +19,29 @@ def login_page_content_visible(driver_instance):
 
 
 def correct_login(driver_instance):
+    wait_for_visibility_of_element(driver_instance, LoginPageLocators.username)
     driver_instance.find_element(*LoginPageLocators.username).send_keys(login_data.correct_username)
     driver_instance.find_element(*LoginPageLocators.password).send_keys(login_data.correct_password)
     driver_instance.find_element(*LoginPageLocators.login_button).click()
 
 
 def incorrect_login(driver_instance):
+    wait_for_visibility_of_element(driver_instance, LoginPageLocators.username)
     driver_instance.find_element(*LoginPageLocators.username).send_keys(login_data.incorrect_username)
     driver_instance.find_element(*LoginPageLocators.password).send_keys(login_data.incorrect_password)
     driver_instance.find_element(*LoginPageLocators.login_button).click()
 
 
-def login_error_visible(driver_instance):
-    element = wait_for_visibility_of_element(driver_instance, LoginPageLocators.login_error_alert)
+def wrong_login_alert_visible(driver_instance):
+    element = wait_for_visibility_of_element(driver_instance, LoginPageLocators.wrong_login_alert)
+    return element.is_displayed()
+
+
+def empty_email_registration_attempt(driver_instance):
+    wait_for_visibility_of_element(driver_instance, LoginPageLocators.register_button)
+    driver_instance.find_element(*LoginPageLocators.register_button).click()
+
+
+def empty_email_alert_visible(driver_instance):
+    element = wait_for_visibility_of_element(driver_instance, LoginPageLocators.empty_email_registration_alert)
     return element.is_displayed()
--- tests/login_test.py
+++ tests/login_test.py
@@ -16,4 +16,9 @@ class LoginTest(BaseTest):
     def test3_incorrect_login(self):
         main_page.go_to_login_page(self.driver)
         login_page.incorrect_login(self.driver)
-        self.assertTrue(login_page.login_error_visible(self.driver))
+        self.assertTrue(login_page.wrong_login_alert_visible(self.driver))
+
+    def test4_empty_email_registration_attempt(self):
+        main_page.go_to_login_page(self.driver)
+        login_page.empty_email_registration_attempt(self.driver)
+        self.assertTrue(login_page.empty_email_alert_visible(self.driver))
